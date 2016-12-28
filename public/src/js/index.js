48384cf595658860f9795ca23d738bdb8f4a2702
liujing <liujing@rongkecloud.com>
2016-12-28 18:39:29 +0800

    1、完善初始

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -6,29 +6,32 @@ let vm = new Vue({
   el: '#app',
   data: {
     kw: '',
-    results:[]
+    results: []
   },
   methods: {
     getResult: function (kw) {
       this.results = [];
       const key = encodeURI(this.kw);
-      this.$http.get('/autocomplete/' + key )
+      this.$http.get('/autocomplete/' + key)
         .then((res) => {
           let resBody = JSON.parse(res.body);
 
-          if( !resBody instanceof Array) return ;
+          if (!resBody instanceof Array) return;
           resBody[0].forEach((res) => {
-            if(res[0] === 'people'){
-              res[3].replace('https','http');
+            if (res[0] === 'people') {
+              res[3].replace('https', 'http');
               vm.results.push({
-                name:res[1],
-                pic:res[3].replace('https','http').replace('_s','_m'),
+                name: res[1],
+                pic: res[3].replace('https', 'http').replace('_s', '_m'),
                 page: 'https://www.zhihu.com/people/' + res[2],
                 content: res[5]
               });
             }
           });
         })
+    },
+    getRelationship: function (userDomain) {
+      
     }
   }
 })
